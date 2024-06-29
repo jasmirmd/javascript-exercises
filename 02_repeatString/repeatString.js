72f7fba6637bb952558ac7c169607cd68ba066d9
jasmirmd <jsmrmdn23@gmail.com>
2024-06-29 17:35:16 -0600

    solved repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+  let result = '';
 
+  for(let i = 0; i < n; i++) {
+    result += str;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
