244b8464fb61753c14368d26501a900cf9fb6e66
jasmirmd <jsmrmdn23@gmail.com>
2024-06-29 19:28:11 -0600

    solved remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...moreArgs) {
+  const result = [];
+  
+  arr.map(i => !moreArgs.includes(i) ? result.push(i) : null);
+
+  return result;
 };
 
 // Do not edit below this line
