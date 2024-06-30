7e2441adcb800b97128eb6b66bf7d10fee147db6
jasmirmd <jsmrmdn23@gmail.com>
2024-06-30 01:08:00 -0600

    solved sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+  const arr = [];
+  for(let i = n1; i <= n2; i++) arr.push(i);
+  const result = arr.reduce((a, b) => a + b);
 
+  if(n1 > 0 && n2 > 0 && typeof(n1) === 'number' && typeof(n2) === 'number') {
+    return result;
+  } else {
+    return "ERROR";
+  }
 };
+
+console.log(sumAll(1, 3));
 
 // Do not edit below this line
 module.exports = sumAll;
