68dc2d9ceccfeb5c1abb9d354e1ae1e521240362
jasmirmd <jsmrmdn23@gmail.com>
2024-07-03 02:14:29 -0600

    solved fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let _n = parseInt(n);
+  if(_n < 0) return 'ERROR';
+  return _n <= 1 ? _n : fibonacci(_n - 1) + fibonacci(_n - 2);
 };
+
+console.log(fibonacci('-1'));
 
 // Do not edit below this line
 module.exports = fibonacci;
