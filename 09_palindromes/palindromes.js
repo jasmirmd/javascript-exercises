9de64c32d91b6033e0d7e1e0d1bc1e90b2cbcc29
jasmirmd <jsmrmdn23@gmail.com>
2024-07-03 00:44:26 -0600

    solved palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function(str) {
+  const _str0 = str.slice().replace(/\W/gi, '').split('').reverse().join('');
+  const _str1 = str.slice().replace(/\W/gi, '').split('').join('');
 
-};
+  return _str0.toLowerCase() === _str1.toLowerCase();
+}
+
+console.log(palindromes('racecar'));
 
 // Do not edit below this line
 module.exports = palindromes;
