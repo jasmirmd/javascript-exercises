5783cdfdca8c0e60c3332348a7ba9d88441e2df2
jasmirmd <jsmrmdn23@gmail.com>
2024-07-03 13:05:15 -0600

    solved findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let ages = [];
+  let maxAge = 0;  
 
+  people.forEach((target) => {
+    ages.push(target.hasOwnProperty('yearOfDeath') ? target.yearOfDeath - target.yearOfBirth : null);
+    maxAge = ages.reduce((a, b) => a < b ? b : a);
+  });
+
+  return people[ages.indexOf(maxAge)];
 };
 
 // Do not edit below this line
