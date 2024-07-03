3e4726e01502033e779384d2d6758b032fb5c30a
jasmirmd <jsmrmdn23@gmail.com>
2024-07-03 00:46:37 -0600

    solved palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function(str) {
   const _str0 = str.slice().replace(/\W/gi, '').split('').reverse().join('');
-  const _str1 = str.slice().replace(/\W/gi, '').split('').join('');
+  const _str1 = str.slice().replace(/\W/gi, '');
 
   return _str0.toLowerCase() === _str1.toLowerCase();
 }
