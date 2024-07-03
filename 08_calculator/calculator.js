88bd87641ed72d153b2028f07ed671a0dea60051
jasmirmd <jsmrmdn23@gmail.com>
2024-07-02 19:16:42 -0600

    solved calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((prev, curr) => prev + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((prev, curr) => prev * curr);
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n1) {
+  return n1 === 0 ? 1 :
+  Array.from({length: n1 + 1})
+  .map((_, i) => i)
+  .filter(x => x > 0)
+  .reduce((prev, curr) => prev * curr);
 };
 
 // Do not edit below this line
